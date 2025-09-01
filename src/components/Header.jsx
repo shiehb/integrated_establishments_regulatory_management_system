1c349fa23735c6f1b3df17c2f0d8523c35eddf98
JERICHO URBANO <119677296+shiehb@users.noreply.github.com>
2025-09-01 23:31:24 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,26 +3,26 @@ import logo from "../assets/logo1.svg";
 export default function Header() {
   return (
     <header className="bg-white shadow-md border-b-1 text-sky-700">
-      <div className="container px-2">
+      <div className="container px-2 py-1">
         <div className="flex flex-col items-center gap-4 md:flex-row md:items-center">
           {/* Logo */}
           <div className="flex-shrink-0">
             <img
               src={logo}
               alt="DENR-EMB Logo"
-              className="object-contain w-14 h-14 md:w-16 md:h-16"
+              className="object-contain w-12 h-12 md:w-12 md:h-14"
             />
           </div>
 
           {/* Text Content */}
           <div className="text-center md:text-left">
-            <h1 className="text-xl font-bold md:text-xl">
+            <h1 className="text-lg font-bold md:text-lg">
               Department of Environmental and Natural Resources
             </h1>
-            <h1 className="text-sm italic font-semibold md:text-base">
+            <h1 className="text-xs italic font-semibold md:text-sm">
               Environmental Management Bureau
             </h1>
-            <h2 className="text-sm font-semibold tracking-wide md:text-sm">
+            <h2 className="text-xs font-semibold tracking-wide md:text-xs">
               Integrated Establishment Regulatory Management System
             </h2>
           </div>
