326914daefbcf0b75dcf0d0c090c00f80fe88104
JERICHO URBANO <119677296+shiehb@users.noreply.github.com>
2025-11-01 15:36:17 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_ENV_PATH || "/integrated_establishments_regulatory_management_system",
 })
